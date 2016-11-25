0f23ba7af875b16fad3810d8f3f059081465cb9c
Yuriy Bogatyrenko <yurabogatyrenko@gmail.com>
2016-11-25 16:57:32 +0600

    reg step 1 static finished

--- app/js/app.js
+++ app/js/app.js
@@ -1,43 +1,13 @@
-(function() {
+$doc = $(document);
 
-function CINDRELLAPROJECT(doc) {
-	var _self = this;
+$(function () {
+	$doc.on('click', '[data-toggle]', function () {
+		var target = $(this).attr('data-toggle');
+		var bl = $('[data-toggle-target="'+target+'"]');
 
-	_self.doc = doc;
-
-	_self.constructor(_self);
-}
-
-CINDRELLAPROJECT.prototype.constructor = function() {
-	var progressBars = this.doc.querySelectorAll('[data-progress-bar]');
-
-	for(var i=0; i<progressBars.length; i++) {
-		progressBars[i].style.width = progressBars[i].dataset.progressBar;
-	}
-
-	$('.owl-carousel').owlCarousel({
-		loop:true,
-		margin:15,
-		responsive:{
-			0:{
-				items:1
-			},
-			600:{
-				items:3
-			},
-			1000:{
-				items:6
-			}
-		}
+		bl.slideToggle(300);
+		$(this).toggleClass('active');
+		console.log(target);
+		console.log(bl);
 	});
-
-	$('[data-scroll]').click(function (e) {
-		$('html, body').stop().animate({
-			scrollTop: $($(this).attr('href')).offset().top
-		}, 1000);
-	});
-};
-
-var app = new CINDRELLAPROJECT(document);
-
-})();
+});
